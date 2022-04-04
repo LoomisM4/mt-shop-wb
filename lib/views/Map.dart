180c8cabce0e2cd1b6044f7ef66a4da058e5c08b
LoomisM4 <mail@marcel-wettach.com>
2022-04-04 15:41:53 +0200

    location can be retreived now

--- lib/views/Map.dart
+++ lib/views/Map.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
-class Map extends StatelessWidget {
+class Map extends StatefulWidget {
   const Map({Key? key}) : super(key: key);
+
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  late Future<LocationData> locationData;
+
+  Future<LocationData> getLocation() async {
+    var location = Location();
+    bool enabled = await location.serviceEnabled();
+    if (!enabled) {
+      enabled = await location.requestService();
+      if (!enabled) {
+        // TODO
+      }
+    }
+    PermissionStatus permission = await location.hasPermission();
+    if (permission == PermissionStatus.denied) {
+      permission = await location.requestPermission();
+      if (permission == PermissionStatus.denied) {
+        // TODO
+      }
+    }
+
+    return location.getLocation();
+  }
+
+  @override
+  void initState() {
+    locationData = getLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +45,23 @@ class Map extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       ),
-      body: const Text("TODO"),
+      body: FutureBuilder<LocationData>(
+        future: locationData,
+        builder: (context, location) {
+          if (location.hasData) {
+            return Column(
+              children: [
+                Text("long " + location.data!.longitude.toString()),
+                Text("lat " + location.data!.latitude.toString()),
+              ],
+            );
+          } else if (location.hasError) {
+            return Text(location.error.toString());
+          } else {
+            return const CircularProgressIndicator();
+          }
+        }
+      )
     );
   }
 }
