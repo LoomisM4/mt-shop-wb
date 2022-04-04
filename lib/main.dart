19773e3f2362d38f111fcf12d33b7f58c45443f5
LoomisM4 <mail@marcel-wettach.com>
2022-04-04 17:47:11 +0200

    tabbar stays visible

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:app/objects/Tabbar.dart';
 import 'package:app/views/Cart.dart';
 import 'package:app/views/Categories.dart';
 import 'package:app/views/Map.dart';
 import 'package:app/views/Spotlight.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -16,20 +16,54 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          body: TabBarView(
-            children: [
-              Spotlight(),
-              Categories(title: "Kategorien"),
-              Cart(),
-              Map()
+    return MaterialApp(
+      home: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.star)),
+              BottomNavigationBarItem(icon: Icon(Icons.list)),
+              BottomNavigationBarItem(icon: Icon(Icons.shopping_cart)),
+              BottomNavigationBarItem(icon: Icon(Icons.map))
             ]
-          ),
-          bottomNavigationBar: Tabbar()
         ),
+        tabBuilder: (context, i) {
+          switch (i) {
+            case 0:
+              return CupertinoTabView(
+                  builder: (context) {
+                    return const CupertinoPageScaffold(
+                        child: Spotlight()
+                    );
+                  }
+              );
+            case 1:
+              return CupertinoTabView(
+                  builder: (context) {
+                    return const CupertinoPageScaffold(
+                        child: Categories(title: "Kategorien")
+                    );
+                  }
+              );
+            case 2:
+              return CupertinoTabView(
+                  builder: (context) {
+                    return const CupertinoPageScaffold(
+                        child: Cart()
+                    );
+                  }
+              );
+            case 3:
+              return CupertinoTabView(
+                  builder: (context) {
+                    return const CupertinoPageScaffold(
+                        child: Map()
+                    );
+                  }
+              );
+            default:
+              return const Text("Es ist ein Fehler aufgetreten");
+          }
+        }
       ),
     );
   }
